cd7818f4b136a4e10c1417a3e8821523e0c7c786
Piotrek <pswirkaitis@wp.pl>
2021-05-13 20:55:01 +0200

    sso init

--- src/main/webapp/app/entities/entity-routing.module.ts
+++ src/main/webapp/app/entities/entity-routing.module.ts
@@ -4,6 +4,11 @@ import { RouterModule } from '@angular/router';
 @NgModule({
   imports: [
     RouterModule.forChild([
+      {
+        path: 'calendar',
+        data: { pageTitle: 'Calendars' },
+        loadChildren: () => import('./calendar/calendar.module').then(m => m.CalendarModule),
+      },
       /* jhipster-needle-add-entity-route - JHipster will add entity modules routes here */
     ]),
   ],
